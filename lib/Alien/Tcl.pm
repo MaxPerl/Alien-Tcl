f30370807e9f4902a0e51bb5e47c0754bf20586d
MaxPerl <max.augsburg@gmx.de>
2017-02-19 17:53:36 +0100

    Add files via upload

---
+++ lib/Alien/Tcl.pm
@@ -0,0 +1,65 @@
+package Alien::Tcl;
+use strict;
+
+use parent 'Alien::Base';
+
+1;
+
+
+
+=head1 NAME
+
+Alien::Tcl - Installs
+
+=head1 SYNOPSIS
+
+  use Alien::Tcl;
+  blah blah blah
+
+
+=head1 DESCRIPTION
+
+Stub documentation for this module was created by ExtUtils::ModuleMaker.
+It looks like the author of the extension was negligent enough
+to leave the stub unedited.
+
+Blah blah blah.
+
+
+=head1 USAGE
+
+
+
+=head1 BUGS
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+    A. U. Thor
+    CPAN ID: MODAUTHOR
+    XYZ Corp.
+    a.u.thor@a.galaxy.far.far.away
+    http://a.galaxy.far.far.away/modules
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+
+#################### main pod documentation end ###################
+
